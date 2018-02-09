2a4a3d2e674494d7a0a6444b11f8d30671d19264
salman mujtaba <salmanmujtaba1793@gmail.com>
2018-02-08 20:55:49 -0500

    Remove edit expense help from header. Add numeral for currency

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import numeral from 'numeral';
+import moment from 'moment';
+
 const ExpenseListItem = (props)=>(
     <div>
         <Link to={`/edit/${props.expense.id}`}>
             <h3>{props.expense.description}</h3>
         </Link> 
-        <p>{props.expense.amount}</p>
-        {props.expense.createdAt}<br/>    
+        <p>
+            {numeral(props.expense.amount).format('$0,0.00')}</p>
+            {moment(props.expense.createdAt).format('MMMM Do, YYYY')}<br/>    
     </div>
 );
 
